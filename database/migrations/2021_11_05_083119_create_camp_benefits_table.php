dbc11e731aeef912a0d3c8bf5aa452291b051f07
rzkzx <romanzidanramadhan@gmail.com>
2021-11-05 19:58:37 +0800

    Migration Camp Benefit Relation

--- database/migrations/2021_11_05_083119_create_camp_benefits_table.php
+++ database/migrations/2021_11_05_083119_create_camp_benefits_table.php
@@ -15,9 +15,17 @@ class CreateCampBenefitsTable extends Migration
     {
         Schema::create('camp_benefits', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('camp_id');
+            //1st method
+            // $table->bigInteger('camp_id')->unsigned();
+            // $table->unsignedBigInteger('camp_id');
+
+            //2nd method
+            $table->foreignId('camp_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->timestamps();
+
+            // 1st method
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
         });
     }
 
